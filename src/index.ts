255e922219b88fd6d49dc3a310840650656f866d
chethanavirajini <cheth.virajini@gmail.com>
2023-10-15 19:48:44 +0530

    transpilation with tsc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 console.log('Hello World');
 
-let age: number = 20;
+let age : number = 20;
 // age = 'a'  error because we cant chnage this variable this features enables more robust and error free code in large projects
 
