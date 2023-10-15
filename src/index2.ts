7a417ffbb1c8220c1b71d9a617ede621fc3254f3
chethanavirajini <cheth.virajini@gmail.com>
2023-10-15 20:19:13 +0530

    primitive datatypes

---
+++ src/index2.ts
@@ -0,0 +1,8 @@
+//primitive type in typescript
+//typescript compiler can detect the type of their variables by its value. 
+// we dont need to annonate them everytime
+
+
+let sales = 123_456_789;
+let course = "typescript";
+let is_published = true;
