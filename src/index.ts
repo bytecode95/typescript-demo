1cbef0ce71331dc6cdc7356cb7401006ada2efdf
chethanavirajini <cheth.virajini@gmail.com>
2023-10-15 20:09:22 +0530

    debugging typescript

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ console.log('Hello World');
 let age : number = 20;
 // age = 'a'  error because we cant chnage this variable this features enables more robust and error free code in large projects
 
+if(age<50)
+  age += 10;
+console.log(age)
