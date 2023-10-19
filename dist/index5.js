d3faa1e1f2a08e1a5927fb22e1a465c1aa8dccaa
chethanavirajini <cheth.virajini@gmail.com>
2023-10-19 23:33:19 +0530

    enum

---
+++ dist/index5.js
@@ -0,0 +1,11 @@
+"use strict";
+;
+var SizeinLetter;
+(function (SizeinLetter) {
+    SizeinLetter["Small"] = "s";
+    SizeinLetter["Medium"] = "m";
+    SizeinLetter["Large"] = "l";
+})(SizeinLetter || (SizeinLetter = {}));
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=index5.js.map
---
+++ src/index5.ts
@@ -0,0 +1,15 @@
+//enum -- list of related constants
+
+// const small = 1;
+// const medium = 2;
+// const large =3;
+
+//PascalCase
+
+//bydefault typescript compiler assigned values to members 0,1,2 
+//if we dont want this default values we can explicitly set values 
+const enum Size {Small = 1, Medium, Large};
+enum SizeinLetter {Small='s', Medium='m', Large='l'}
+
+let mySize: Size = Size.Medium;
+console.log(mySize)
