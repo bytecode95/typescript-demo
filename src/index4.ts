bc20e9155e7f2886a79f35060d7eb303a236822c
chethanavirajini <cheth.virajini@gmail.com>
2023-10-20 22:04:23 +0530

    code completion

--- src/index4.ts
+++ src/index4.ts
@@ -1,5 +1,6 @@
-//tuples  -- which is a fixed length array whwre each element as partuclar type
+//tuples  -- which is a fixed length array whwre each element as particular type
 //we often use this when working with pair of values
 //when we have use present user with two variables id & name key value pairs
 
 let user: [number, string] = [1, 'Mosh'];
+//user[1]. code completeion
