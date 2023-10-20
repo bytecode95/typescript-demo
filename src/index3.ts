b1062874f40d4850cbdcfc8d0876735a43a18b26
chethanavirajini <cheth.virajini@gmail.com>
2023-10-20 22:01:33 +0530

    code completion

--- src/index3.ts
+++ src/index3.ts
@@ -4,4 +4,4 @@ let numbers = [1,2,3];
 
 let count = [];
 //if you need to use an empty array you have to expliceilty notation what type of that array
-//foreach usage to typecript array
+//foreach usage to typecript array - code completion
