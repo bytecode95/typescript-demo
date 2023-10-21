ab8204eb732069fbf7d5b5ed23e79490dd713789
chethanavirajini <cheth.virajini@gmail.com>
2023-10-21 19:29:36 +0530

    intersection

---
+++ src/index10.ts
@@ -0,0 +1,5 @@
+//intersection types
+//using union type  variable or function parameter one of many types
+//another technique to combining types called intersection
+
+let weight: number & string;
