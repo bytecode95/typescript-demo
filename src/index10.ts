41cf92a2a70d319fe4484d2ecb3cf91590fbbcb9
chethanavirajini <cheth.virajini@gmail.com>
2023-10-23 20:42:02 +0530

    intersection type

--- src/index10.ts
+++ src/index10.ts
@@ -2,4 +2,16 @@
 //using union type  variable or function parameter one of many types
 //another technique to combining types called intersection
 
-let weight: number & string;
+type Draggable = {
+    drag: () => void
+}
+
+type Resizable = {
+    resize: () => void
+}
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
