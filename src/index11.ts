90d417f40e320a678218a225b97f552fd1ab4049
chethanavirajini <cheth.virajini@gmail.com>
2023-10-23 21:05:44 +0530

    literal types

---
+++ src/index11.ts
@@ -0,0 +1,11 @@
+//Literal Types
+//in order to limit value of variable
+//Ex: let quantity: number; --- this variable can get any number
+
+//define type alias
+type Quantity = 50 | 100;
+
+let quantity: Quantity  = 50;
+
+type Metric = 'cm' | 'inch';
+ let distance: Metric = 'inch';
