dc454e09e9b07f137606ccfd9ef5d3f2fba05d4e
chethanavirajini <cheth.virajini@gmail.com>
2023-10-27 10:47:18 +0530

    special type in typescript

--- src/index12.ts
+++ src/index12.ts
@@ -1,6 +1,13 @@
 //Nullable types
 // typescript is very strict using null & undeifned
 
+//TypeScript special type variables
+// 1. any
+// 2. Never
+// 3. Nullable
+// 4. Undefined
+// 5. Unknown
+
 function greet(name: string | null | undefined){
     if(name)
     console.log(name.toUpperCase());
