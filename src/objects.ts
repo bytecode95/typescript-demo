5a95d2892829dea062208a69fadcdecbef16903c
chethanavirajini <cheth.virajini@gmail.com>
2023-12-01 14:43:52 +0530

    object

---
+++ src/objects.ts
@@ -0,0 +1,36 @@
+//objects
+
+//define interface for a person obejct
+
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+//Create an object that adhere to the Person interface
+
+const person:Person = {
+    firstName:'John',
+    lastName:'Doe',
+    age:20
+};
+
+//access properties
+console.log(person.firstName);
+console.log(person.age);
+
+
+interface Car {
+    make: string;
+    model: string;
+    year?: number;
+    readonly vin: string;
+}
+
+const myCar: Car = {
+    make:"Toyota",
+    model:"Camrey",
+    year: 2020,
+    vin: "ABCGFD"
+}
